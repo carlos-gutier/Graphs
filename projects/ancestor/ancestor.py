127bc5c6d87df477cc344088a8ce0263763de47a
Carlos Gutierrez <chgutier@gmail.com>
2019-06-19 16:31:37 -0500

    Best ancestor.py solution

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,60 +1,61 @@
-from collections import deque
-
-
-class Stack():
+class Queue():
     def __init__(self):
-        self.stack = deque()
-
-    def push(self, value):
-        self.stack.append(value)
-
-    def pop(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
         if self.size() > 0:
-            return self.stack.pop()
-
+            return self.queue.pop(0)
+        else:
+            return None
     def size(self):
-        return len(self.stack)
+        return len(self.queue)
+
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+
 
 
 def earliest_ancestor(ancestors, starting_node):
-    graph = dict()
-    for tpl in ancestors:
-        if tpl[1] not in graph:
-            graph[tpl[1]] = set()
-        graph[tpl[1]].add(tpl[0])
+    # Build the graph
+    graph = Graph()
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        # (build edges in reverse)
+        graph.add_edge(pair[1], pair[0])
+    # (track the longest path length and the earliest ancestor node)
+    max_path_len = 1
+    earliest_ancestor = -1
+    # Do a BFS from starting_node to each other node
+    q = Queue()
+    q.enqueue([starting_node])
+    while q.size() > 0:
+        path = q.dequeue()
+        v = path[-1]
+        # If Path is longer or path is same length and node is smaller
+        if (len(path) == max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+        for neighbor in graph.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            q.enqueue(path_copy)
+    # Return the earliest ancestor
+    return earliest_ancestor
 
-    def solve(starting_vertex):
-        nonlocal graph
-        if starting_vertex not in graph:
-            return -1
 
-        distances = dict()
-
-        def dfs(starting_vertex):
-            visited = set()
-            stack = Stack()
-            stack.push([starting_vertex])
-            while stack.size() > 0:
-                path = stack.pop()
-                vertex = path[-1]  # last element in the array storing the path
-                if vertex not in visited and vertex in graph:
-                    for neighbor in graph[vertex]:
-                        path_new = path[:]  # path.copy()
-                        path_new.append(neighbor)
-                        stack.push(path_new)
-                        if neighbor not in graph:
-                            callback(neighbor, path_new)
-                            continue
-                    visited.add(vertex)
-
-        def callback(neighbor, path_new):
-            distances[neighbor] = len(path_new)
-        
-        dfs(starting_vertex)
-        return min([k for k,v in distances.items() if v == max([v for k,v in distances.items()])])
-    return solve(starting_node)
 
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-# print(make_adjacency_list(test_ancestors))
 print(earliest_ancestor(test_ancestors, 6))
