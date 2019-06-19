591b5b324a43650f2aba3033315c74c6fbf6be9f
Carlos Gutierrez <chgutier@gmail.com>
2019-06-19 15:49:04 -0500

    Working ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,79 +1,60 @@
-class Queue():
+from collections import deque
+
+
+class Stack():
     def __init__(self):
-        self.queue = []
-    def enqueue(self, value):
-        self.queue.append(value)
-    def dequeue(self):
+        self.stack = deque()
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
         if self.size() > 0:
-            return self.queue.pop(0)
-        else:
-            return None
+            return self.stack.pop()
+
     def size(self):
-        return len(self.queue)
+        return len(self.stack)
 
 
-class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    def __init__(self):
-        # dictionary of vertices as "keys" and
-        # "set" of edges (vertices) as "values" (see add_vertex)
-        self.vertices = {} 
+def earliest_ancestor(ancestors, starting_node):
+    graph = dict()
+    for tpl in ancestors:
+        if tpl[1] not in graph:
+            graph[tpl[1]] = set()
+        graph[tpl[1]].add(tpl[0])
 
-    def add_vertex(self, ancestors_list):
-        """
-        Add a vertex to the graph.
-        """
-        # this is to add all vertices as keys first
-        for v in ancestors_list:
-            self.vertices[v] = set()
+    def solve(starting_vertex):
+        nonlocal graph
+        if starting_vertex not in graph:
+            return -1
 
+        distances = dict()
 
-    def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
-        # adding values (set) to keys already added
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
-        else:
-            # cannot add edges if they are not vertices first
-            raise IndexError("That vertex does not exist!")
-
-
-    def bfs(self, starting_vertex, destination_vertex):
-            """
-            Return a list containing the SHORTEST PATH from
-            starting_vertex to destination_vertex in
-            breath-first order.
-            """
-            # Create an empty set to store visited nodes
+        def dfs(starting_vertex):
             visited = set()
-            # Create an empty Queue & enqueue A PATH TO the starting vertex
-            q = Queue()
-            q.enqueue( [starting_vertex] ) # "[]" to enqueue PATH
-            # While the queue is not empty...
-            while q.size() > 0:
-                # Dequeue the first PATH
-                path = q.dequeue()
-                # GRAB THE VERTEX FORM THE END OF THE PATH
-                v = path[-1]
-                # IF VERTEX == TARGET, RETURN PATH
-                if v == destination_vertex:
-                    return path
-                # if that vertex has not been visited...
-                if v not in visited:
-                    # Mark it as visited
-                    visited.add(v)
-                    # Then add a path to all of its neighbors to the back of the queue
-                    for neighbor in self.vertices[v]:
-                        # Copy the path
-                        path_copy = list(path)
-                        # Append neighbor to the back of the copy
-                        path_copy.append(neighbor)
-                        # Enqueue copy
-                        q.enqueue(path_copy)
+            stack = Stack()
+            stack.push([starting_vertex])
+            while stack.size() > 0:
+                path = stack.pop()
+                vertex = path[-1]  # last element in the array storing the path
+                if vertex not in visited and vertex in graph:
+                    for neighbor in graph[vertex]:
+                        path_new = path[:]  # path.copy()
+                        path_new.append(neighbor)
+                        stack.push(path_new)
+                        if neighbor not in graph:
+                            callback(neighbor, path_new)
+                            continue
+                    visited.add(vertex)
 
-
+        def callback(neighbor, path_new):
+            distances[neighbor] = len(path_new)
+        
+        dfs(starting_vertex)
+        return min([k for k,v in distances.items() if v == max([v for k,v in distances.items()])])
+    return solve(starting_node)
 
 
 test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+# print(make_adjacency_list(test_ancestors))
+print(earliest_ancestor(test_ancestors, 6))
